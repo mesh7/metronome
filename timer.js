25ce3b7aa372609658bf0b0040e0572d2f63f226
Shubham Meshram <meshram.shubham27@gmail.com>
2022-06-12 00:30:54 +0530

    added changes for timer function

--- timer.js
+++ timer.js
@@ -15,17 +15,14 @@ function Timer(callback, timeInterval, options) {
       } 
       
       this.timeout = setTimeout(this.round, this.timeInterval);
-      console.log('Timer Started');
     }
     // Add method to stop timer
     this.stop = () => {
   
       clearTimeout(this.timeout);
-      console.log('Timer Stopped');
     }
     // Round method that takes care of running the callback and adjusting the time
     this.round = () => {
-      console.log('timeout', this.timeout);
       // The drift will be the current moment in time for this round minus the expected time..
       let drift = Date.now() - this.expected;
       // Run error callback if drift is greater than time interval, and if the callback is provided
@@ -38,8 +35,6 @@ function Timer(callback, timeInterval, options) {
       callback();
       // Increment expected time by time interval for every round after running the callback function.
       this.expected += this.timeInterval;
-      console.log('Drift:', drift);
-      console.log('Next round time interval:', this.timeInterval - drift);
       // Run timeout again and set the timeInterval of the next iteration to the original time interval minus the drift.
       this.timeout = setTimeout(this.round, this.timeInterval - drift);
     }
